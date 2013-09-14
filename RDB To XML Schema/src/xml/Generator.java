87beff1e3cefc7dfd5b33b10b4ce5ea25bbdbe5a
janicechowwx <Janice@Janice-MacBook-Pro.local>
2013-09-14 13:55:05 +0800

    Add Java Project

---
+++ RDB To XML Schema/src/xml/Generator.java
@@ -0,0 +1,9 @@
+package xml;
+
+import java.io.File;
+
+public interface Generator {
+	
+	public File generate(String dbName, String fileName);
+	
+}
---
+++ RDB To XML Schema/src/xml/XMLDataGenerator.java
@@ -0,0 +1,15 @@
+package xml;
+
+import java.io.File;
+
+public class XMLDataGenerator implements Generator {
+
+	@Override
+	public File generate(String dbName, String fileName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+}
