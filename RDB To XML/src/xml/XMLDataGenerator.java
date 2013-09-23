f667b70837a6a40916f1f0e72ac1f491ab155bec
z-liang <z.liang90@gmail.com>
2013-09-23 11:54:40 +0800

    Reference to xml schema added

--- RDB To XML/src/xml/XMLDataGenerator.java
+++ RDB To XML/src/xml/XMLDataGenerator.java
@@ -11,71 +11,78 @@ import java.util.List;
 import java.util.Iterator;
 import javax.sql.rowset.CachedRowSet;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import database.DBAccess;
+import main.MainException;
 
 public class XMLDataGenerator implements Generator {
 	@Override
-	public File generate(String dbName, String fileName) {
+	public void generate(String dbName, String fileName) throws MainException {
 		// TODO Auto-generated method stub
-		return privateGenerator(dbName,fileName);
+		privateGenerator(dbName,fileName);
 	}
+	
+	private void privateGenerator(String dbName, String fileName) throws MainException{
 
-	private File privateGenerator(String dbName, String fileName){
-
-		File f = null;
-		try {
-			// Create file to write XML data
-			f = new File(fileName);
-			f.mkdirs();
-			try{
-				f.createNewFile();
-			}catch(IOException e){
-				e.printStackTrace();
-			}
-			PrintWriter writer = new PrintWriter(new FileOutputStream(fileName),true);
-			// Write DB name to file
-			writer.println("<" + dbName.toUpperCase() + ">");
-
-			try{
-				// Get table names & data
-				DBAccess dbCache = DBAccess.getInstance();
-				Map<String, CachedRowSet> tables = dbCache.getTableCache();
-				Set<String> tablenames = tables.keySet();
-				Iterator<String> tableItr = tablenames.iterator();
-
-				// Write data for each table/relation
-				while(tableItr.hasNext()){
-					String tName = tableItr.next();
-					// Write table name to file
-					writer.println("	<"+tName.toUpperCase()+">");
-					
-					// Process the rows for each table
-					CachedRowSet crs = dbCache.getData(tName);
-					List<String> cols = dbCache.getUniqueColumns(tName);
-					while (crs.next()){
-						ResultSet row = crs.getOriginalRow();
-						for(int i=0;i<cols.size();i++){
-							String colName = cols.get(i);
-							writer.print("		<"+colName.toUpperCase()+">");
-							String nextData = row.getNString(colName);
-							writer.println(nextData+"</"+colName.toUpperCase()+">");
-						}
-						row.close();
-					}
-					// Write the closing tag for the relation
-					writer.println("	</"+tName.toUpperCase()+">");
-					crs.close();
-				}
-
-				// Write the closing tag for DB
-				writer.println("</"+ dbName.toUpperCase()+">");
-			}catch(Exception ex){
-				ex.printStackTrace();
-			}
-			writer.close();
-		} catch(FileNotFoundException e){
-			e.printStackTrace();
+		String filePath = fileName + ".xml";
+		PrintWriter  writer = null;
+		// Create file to write XML data
+		File f = new File(filePath);
+		f.mkdirs();
+		try{
+			f.createNewFile();
+		}catch(IOException e){
+			throw new MainException("IOException: The data output file cannot be created.");
 		}
-		return f;
+		
+		try{
+			writer = new PrintWriter(new FileOutputStream(filePath),true);
+		}  catch(FileNotFoundException e){
+			throw new MainException("FileOutputStream: Cannot find the data output file.");
+		}
+		// Write xml version info.
+		writer.println("<?xml version=\"1.0\"?>");
+		
+		// Write DB name to file
+		writer.println("<" + dbName);
+		writer.println("xmlns=\"http://www.w3schools.com\"");
+		writer.println("xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"");
+		writer.print("xsi:schemaLocation="+fileName+".xsd>");
+
+		// Get table names & data
+		DBAccess dbCache = DBAccess.getInstance();
+		Map<String, CachedRowSet> tables = dbCache.getTableCache();
+		Set<String> tablenames = tables.keySet();
+		Iterator<String> tableItr = tablenames.iterator();
+		try{
+			// Write data for each table/relation
+			while(tableItr.hasNext()){
+				String tName = tableItr.next();
+				// Write table name to file
+				writer.println("	<"+tName+">");
+
+				// Process the rows for each table
+				CachedRowSet crs = dbCache.getData(tName);
+				List<String> cols = dbCache.getUniqueColumns(tName);
+				while (crs.next()){
+					ResultSet row = crs.getOriginalRow();
+					for(int i=0;i<cols.size();i++){
+						String colName = cols.get(i);
+						writer.print("		<"+colName+">");
+						String nextData = row.getNString(colName);
+						writer.println(nextData+"</"+colName+">");
+					}
+					row.close();
+				}
+				// Write the closing tag for the relation
+				writer.println("	</"+tName+">");
+				crs.close();
+			}
+		}catch(SQLException ex){
+			throw new MainException("DataGenerator: Exception in retrieve data for individual tables.");
+		}
+		// Write the closing tag for DB
+		writer.println("</"+ dbName+">");
+		writer.close();
 	}
 }
