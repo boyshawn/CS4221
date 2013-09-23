6d1417ae5fe894feb5b0f66f683a8315774dc8b9
boyshawn <boyshawn@hotmail.com>
2013-09-23 09:32:32 +0800

    Non-working version: Implemented open Connection, closed Connection
    Untested code.

--- RDB To XML/src/database/DBConnector.java
+++ RDB To XML/src/database/DBConnector.java
@@ -1,25 +1,63 @@
 package database;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnector {
-	
-	private DBConnector instance;
+
+	private DBConnector dbConnector;
 	private Connection dbConnection;
-	
+
+	/**
+	 * The DBConnector is a class which the DB use to deal with connection with database
+	 * @author Francis Pang
+	 *
+	 */
 	private DBConnector() {
 		// stub
 	}
-	
+
 	public DBConnector getInstance() {
 		return null; //stub
 	}
-	
-	public void openConnection(String address, String port, String dbName, String username, String password) {
-		// stub
+
+	/**
+	 * This method will open the database connection with the method with the information given.
+	 * @param address
+	 * @param port
+	 * @param dbName
+	 * @param username
+	 * @param password
+	 * @throws SQLException 
+	 */
+	public void openConnection(String address, String port, String dbName,
+			String username, String password) throws SQLException {
+
+		String connectionUrl = "jdbc:" + address;
+
+		try {
+			dbConnection = DriverManager.getConnection(connectionUrl, username, password);
+		} catch (SQLException e){
+			System.out.println("Login not successful.");
+			throw e;
+		}
+
+		DatabaseMetaData dbMetaData = dbConnection.getMetaData();
 	}
-	
-	public void closeConnection() {
-		// stub
+
+	/**
+	 * Close the Database connection established for this session.
+	 * @throws SQLException 
+	 * 
+	 */
+	public void closeConnection() throws SQLException {
+		try{
+			dbConnection.close();
+		} catch (SQLException e){
+			System.out.println("Error with closing connection");
+			throw e;
+		}
 	}
 }
