b5189aad3aa699e2ec9f8f70df69ffe3b3451073
boyshawn <boyshawn@hotmail.com>
2013-10-05 18:20:21 +0800

    Implement new method in ERD Node (Untested)
    Implement removeLink(ErdNode)

--- RDB To XML/src/erd/ErdNode.java
+++ RDB To XML/src/erd/ErdNode.java
@@ -3,6 +3,8 @@ package erd;
 import java.util.Iterator;
 import java.util.Vector;
 
+import main.MainException;
+
 /**
  * ERD Node Model object.
  * 
@@ -17,26 +19,26 @@ import java.util.Vector;
  * 
  */
 public abstract class ErdNode {
-	
+
 	//Attributes
 	/** The name of the Database table, also known as the <i>relation name</i>. **/
 	String tableName;
 	String originalTableName;
-	
+
 	/**
 	 * To store all the arcs branching out from the ERD node. The connecting arc
 	 * from the ERD node can be either a Entity type or a Relationship type.
 	 */
 	Vector<ErdNode> link; 
-	
+
 	/** Sole constructor**/
 	public ErdNode(String tableName, String originalTableName) {
 		this.tableName = tableName;
 		this.originalTableName = originalTableName;
 	}
-	
+
 	//Method
-	
+
 	/**
 	 * Changes the table name of this ERD Node to be equal to the argument name.
 	 * @param tableName the new table name for this ERD Node.
@@ -44,7 +46,7 @@ public abstract class ErdNode {
 	public void setTableName(String tableName){
 		this.tableName = tableName;
 	}
-	
+
 	/**
 	 * Return the table name of the ERD node
 	 * @return the table name of the ERD node
@@ -52,7 +54,7 @@ public abstract class ErdNode {
 	public String getTableName(){
 		return this.tableName;
 	}
-	
+
 	/**
 	 * Set the original table name of this ERD Node to be equal to the argument name.
 	 * @param originalTableName the original table name for this ERD Node.
@@ -60,7 +62,7 @@ public abstract class ErdNode {
 	public void setOriginalTableName(String originalTableName){
 		this.originalTableName = originalTableName;
 	}
-	
+
 	/**
 	 * Return the original table name of the ERD node
 	 * @return the original table name of the ERD node
@@ -68,7 +70,7 @@ public abstract class ErdNode {
 	public String getOriginalTableName(){
 		return originalTableName;
 	}
-	
+
 	/**
 	 * Adds the ERD Node that an arc from this ERD Node is connected to.
 	 * @param erdNode The ERD Node that this ERD Node is connected to
@@ -76,7 +78,7 @@ public abstract class ErdNode {
 	public void addLink(ErdNode erdNode){
 		link.addElement(erdNode);
 	};
-	
+
 	/**
 	 * Returns the list of ERD Node that this ERD Node is connected to
 	 * @return the list of ERD Node that this ERD Node is connected to
@@ -84,7 +86,7 @@ public abstract class ErdNode {
 	public Vector<ErdNode> getLinks(){
 		return this.link;
 	}
-	
+
 	/**
 	 * check if the ERD Node and the ERD Node in question is connected
 	 * @param tableName table name of the ERD Node we are checking 
@@ -92,21 +94,38 @@ public abstract class ErdNode {
 	 */
 	public boolean isConnectedto(String tableName){
 		Iterator<ErdNode> linkIterator = link.iterator();
-		
+
 		if(!linkIterator.hasNext()){
 			return false;
 		}
-		
+
 		do{
 			ErdNode iterErdNode = (ErdNode) linkIterator.next();
-			
+
 			String erdNodeTableName = iterErdNode.getTableName();
-			
+
 			if(erdNodeTableName == tableName){
 				return true;
 			}
 		}while(linkIterator.hasNext());
-		
+
 		return false;
+	}
+
+	/**
+	 * Removes the arc from this ERD Node to the specified ERD Node
+	 * @param erdNode the arc to the ERD Node to be disconnected 
+	 * @throws Exception if the ERD Node is not connected to this ERD Node
+	 */
+	public void removeLink(ErdNode erdNode) throws Exception{
+		//To check that there list contain the node to be removed		
+		if(!this.link.remove(erdNode)){
+			final String ErdNodeNotInsideLinkExceptionMessage = "The node "
+					+ erdNode.getTableName()
+					+ "is not inside the link of ERD Node " + this.tableName
+					+ ".";
+
+			throw new MainException(ErdNodeNotInsideLinkExceptionMessage);
+		}
 	}
 }
